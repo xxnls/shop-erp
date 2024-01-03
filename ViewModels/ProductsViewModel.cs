762880c4076d3c733f91156de9489b9e25a0c15e
Bartosz Rosiek <bartosz.rosiek666@gmail.com>
2024-01-03 02:22:05 +0100

    Created GetModels for ProductsViewModel

--- ViewModels/ProductsViewModel.cs
+++ ViewModels/ProductsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ShopERP.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopERP.Models;
+using ShopERP.Models.Contexts;
 using ShopERP.ViewModels.BaseViewModels;
 using ShopERP.ViewResources;
 using System;
@@ -12,25 +14,49 @@ namespace ShopERP.ViewModels
 {
     public class ProductsViewModel : BaseObjectViewModel<Product>
     {
+        #region Properties and Fields
+        public ObservableCollection<ProductCategory> ProductCategories { get; set; }
+        public ObservableCollection<ProductUnit> ProductUnits { get; set; }
 
+        #endregion
         public ProductsViewModel() : base(GlobalResources.Products)
         {
+            ProductCategories = new ObservableCollection<ProductCategory>(GetProductCategories());
+            ProductUnits = new ObservableCollection<ProductUnit>(GetProductUnits());
         }
         public override void Delete()
         {
-            throw new NotImplementedException();
+            
         }
 
         public override void Edit()
         {
-            throw new NotImplementedException();
+            
         }
-
         public override IEnumerable<Product> GetModels()
         {
-            throw new NotImplementedException();
+            using (var dbContext = new DatabaseContext())
+            {
+                return dbContext.Products.Include(item => item.ProductCategory)
+                                         .Include(item => item.ProductUnit)
+                                         .Where(address => address.DateDeleted == null)
+                                         .ToList();
+            }
         }
-
+        private IEnumerable<ProductCategory> GetProductCategories()
+        {
+            using (var dbContext = new DatabaseContext())
+            {
+                return dbContext.ProductCategories.ToList();
+            }
+        }
+        private IEnumerable<ProductUnit> GetProductUnits()
+        {
+            using (var dbContext = new DatabaseContext())
+            {
+                return dbContext.ProductUnits.ToList();
+            }
+        }
         public override void Refresh()
         {
             throw new NotImplementedException();
